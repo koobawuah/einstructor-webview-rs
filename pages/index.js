68fb49326c90714dc27d7e54cd723ac84980711f
koobawuah <me@bawuahboakye.com>
2022-12-29 11:53:59 +0000

    Init commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Express Instructor Lesson contents" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Express Instructor Lesson content</h1>
+      </main>
+    </>
+  );
+}
